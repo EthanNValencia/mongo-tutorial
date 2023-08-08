37c7f1b046676e073ca0350960c2901601f06be2
EthanNValencia <enephew2@mail.valenciacollege.edu>
2023-08-08 11:24:42 -0400

    Init upload. Got the props done.

---
+++ src/main/java/com/nephew/tutorial/mongotutorial/MongoTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.nephew.tutorial.mongotutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nephew/tutorial/mongotutorial/MongoTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nephew.tutorial.mongotutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
